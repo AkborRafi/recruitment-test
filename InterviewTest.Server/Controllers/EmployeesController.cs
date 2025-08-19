de87d7e26a9d8270c2434d58264c55728a7f16d8
AkborRafi <akbor.rafi@outlook.com>
2025-08-19 03:18:15 +0100

    EmployeeController login is working; ListController API is under review
    Enhance EmployeesController and ListController comments
    
    Updated EmployeesController with ADO.NET context comments and improved column index retrieval using GetOrdinal. Clarified connection management comments for production use.
    
    Modified ListController to include a private logger field and added commented-out API methods for incrementing values and calculating sums based on name prefixes, currently under review.

--- InterviewTest.Server/Controllers/EmployeesController.cs
+++ InterviewTest.Server/Controllers/EmployeesController.cs
@@ -11,6 +11,17 @@ namespace InterviewTest.Server.Controllers
         [HttpGet]
         public List<Employee> Get()
         {
+
+            //ADO.NET have individual types such as SqlConnection, SqliteConnection, OracleConnection etc.
+            //SqliteConnection is used to connect to Sqlite database.
+            //SqlConnection is used to connect to Sql Server database.
+            //SqlConnection
+            //SqlCommand
+            //SqlAdapter
+            //SqlDataReader
+            //SqlDataAdapter
+
+
             var employees = new List<Employee>();
 
             var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
@@ -22,15 +33,40 @@ namespace InterviewTest.Server.Controllers
                 queryCmd.CommandText = @"SELECT Name, Value FROM Employees";
                 using (var reader = queryCmd.ExecuteReader())
                 {
+
+                    //**Note: GetOrdinal is used to get the value of a column by its name.**// 
+                    
+                    //Get the collumn index of the "Name" column
+                    var nameOrdinal = reader.GetOrdinal("Name");
+
+                    //Get the collumn index of the "Value" column
+                    var valueOrdinal = reader.GetOrdinal("Value");
+
                     while (reader.Read())
                     {
                         employees.Add(new Employee
                         {
-                            Name = reader.GetString(0),
-                            Value = reader.GetInt32(1)
+                            //C# are equivalent of NVARCHAR(Name) where Name is declare by string
+                            //**Name = reader.GetString(0),
+                            Name = reader.GetString(nameOrdinal),
+
+                            //C# are equivalent of INT(Value) where Value is declare by integer (Int32)
+                            //Value = reader.GetInt32(1)
+                            Value = reader.GetInt32(valueOrdinal)
                         });
                     }
                 }
+
+
+                /*
+                -----------------------------
+                *No need to use this code in production!
+                *It is only for demonstration purposes.
+                *It is already using by using statement. (line:28)
+                ------------------------------
+                connection.Close();
+                connection.Dispose();
+                 */
             }
 
             return employees;
--- InterviewTest.Server/Controllers/ListController.cs
+++ InterviewTest.Server/Controllers/ListController.cs
@@ -9,15 +9,116 @@ namespace InterviewTest.Server.Controllers
         // This controller is intended to handle list-related API methods.
         //Only allowed to read data from the database.
 
+        private readonly ILogger<ListController> _logger;
         
 
         public ListController()
         {
-           
+
         }
 
         /*
          * List API methods goe here
          * */
+
+
+        //Summary for the API method
+
+        //Increment Value of the list item
+        //If name starts with 'E', increment by +1
+        //If name starts with 'G', increment by +10
+        //All other names increment by +100
+
+
+        /*************** Under Review ****************/
+        /*[HttpPost("increment")]
+        public async Task<IActionResult> IncrementValues()
+        {
+            var employees = await_context.Employees.ToListAsync();
+
+            foreach(var e in employees)
+            {   if (e.Name.StartsWith("E", StringComparison.OrdinalIgnoreCase))
+                {
+                    e.Value += 1;
+                }
+                else if (e.Name.StartsWith("G", StringComparison.OrdinalIgnoreCase))
+                {
+                    e.Value += 10;
+                }
+                else
+                {
+                    e.Value += 100;
+                }
+            }
+
+            await _context.SaveChangesAsync();
+
+            //return updated list so frontend get refreshes
+
+            return Ok(emplyees);
+        }*/
+
+        /*
+         <summary>
+         *Get the sum of all values in the list which starts with 'A','B','C'
+         *Only return if the sum is greater than or equal; >=11171 
+         *Otherwise return NotFound
+         </summary>
+         */
+        /*
+        [HttpGet("sum-abc")]
+        public async Task<IActionResult> GetSumABC()
+        {
+            var sum = await_context.Employees
+                .Where(e => e.Name.StartsWith("A", StringComparison.OrdinalIgnoreCase) ||
+                            e.Name.StartsWith("B", StringComparison.OrdinalIgnoreCase) ||
+                            e.Name.StartsWith("C", StringComparison.OrdinalIgnoreCase))
+                .SumAsync(e => e.Value);
+
+            if (sum >= 11171)
+                return Ok (new { Sum = sum });
+
+            // If sum is less than 11171, message will show return NotFound
+            return Ok(new {Message = "No result >= 11171" });
+        }*/
+
+
+        /*
+        public async Task<IActionResult> IncrementValue(string name)
+        {
+            // Logic to increment the value based on the name prefix
+            int incrementValue = 100; // Default increment value
+            
+            if (name.StartsWith("E"))
+            {
+                incrementValue = 1;
+            }
+            else if (name.StartsWith("G"))
+            {
+                incrementValue = 10;
+            }
+            // Here you would typically update the database or data source with the new value
+            // For demonstration, we will just return the incremented value
+            
+            return Ok(new { Name = name, IncrementedValue = incrementValue });
+        }
+        */
+
+        /*
+        public async Task<IActionResult> GetSumABC()
+        {
+            var employees = await _context.Employees.ToListAsync();
+            var sum = employees
+                .Where(e => e.Name.StartsWith("A", StringComparison.OrdinalIgnoreCase) ||
+                            e.Name.StartsWith("B", StringComparison.OrdinalIgnoreCase) ||
+                            e.Name.StartsWith("C", StringComparison.OrdinalIgnoreCase))
+                .Sum(e => e.Value);
+            if (sum >= 11171)
+            {
+                return Ok(new { Sum = sum });
+            }
+            return NotFound("Sum is less than 11171");
+        }
+        */
     }
 }
